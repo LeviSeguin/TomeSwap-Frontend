7f6a2d26c13930c1e42446fecac5ccd8e1f83607
LeviSeguin <86278004+LeviSeguin@users.noreply.github.com>
2024-02-25 10:40:42 -0500

    added the test fetch api

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,19 +1,49 @@
+import React from 'react';
 import Header from './components/Header.js';
 import Footer from './components/Footer.js';
 import HorizontalScrollList from './components/HorizontalScrollList.js';
 
-const items = [
-  <div className="item-content">Item 1</div>,
-  <div className="item-content">Item 2</div>,
-  <div className="item-content">Item 3</div>,
-  <div className="item-content">Item 4</div>,
-  <div className="item-content">Item 5</div>,
-  <div className="item-content">Item 5</div>,
-  <div className="item-content">Item 5</div>,
-];
+//testing fetch
+async function fetchData() {
+  try {
+    const response = await fetch('http://127.0.0.1:8000/api/test/');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    return null;
+  }
+}
+
 
 function App() {
+
+  let fetchedData;
+    // Fetch data when component mounts
+    React.useEffect(() => {
+      fetchData().then(data => {
+        console.log('Fetched data:', data);
+        console.log('just content:', data.content)
+        fetchedData = "test";
+        // Handle data here
+      });
+    }, []); // Empty dependency array to run effect only once on component mount
+
+    const items = [
+      <div className="item-content"><p>{fetchedData || 'Loading...'}</p></div>,
+      <div className="item-content">Item 2</div>,
+      <div className="item-content">Item 3</div>,
+      <div className="item-content">Item 4</div>,
+      <div className="item-content">Item 5</div>,
+      <div className="item-content">Item 5</div>,
+      <div className="item-content">Item 5</div>,
+    ];
+
   return (
+    
     <div className="App">
       <Header />
       <div className="list">
@@ -28,5 +58,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
