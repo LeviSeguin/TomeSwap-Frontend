095488f371af4062b67777d043a3681a6cf220c4
cafreyap <cafreyap@gmail.com>
2024-03-28 04:15:16 -0400

    commit

--- react-frontend/src/components/SignIn.js
+++ react-frontend/src/components/SignIn.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Header from './Header.js';
 import Footer from './Footer.js';
 import SignInForm from './SignInForm.js';
 import CreateAccountForm from './CreateAccountForm.js';
-import '../styles/SignIn.css'
-
-//TODO: switch to css modules, currently the css for SignInForm and CreateAccountForm overlap
+import '../styles/SignIn.css';
 
 const SignIn = () => {
 
@@ -13,24 +11,21 @@ const SignIn = () => {
     <div className="App">
       <Header />
       <div className="signInPage">
-
-          <div className="signInContainer">
-            <div className="signInSection">
-              <h2>Sign In</h2>
-              <SignInForm />
-            </div>
-            <div className="verticalLine"></div>
-            <div className="createAccountSection">
-              <h2>Create Account</h2>
-              <CreateAccountForm />
-            </div>
+        <div className="signInContainer">
+          <div className="signInSection">
+            <h2>Sign In</h2>
+            <SignInForm />
           </div>
-
+          <div className="verticalLine"></div>
+          <div className="createAccountSection">
+            <h2>Create Account</h2>
+            <CreateAccountForm />
+          </div>
+        </div>
       </div>
       <Footer />
-
     </div>
   );
-}
+};
 
 export default SignIn;
