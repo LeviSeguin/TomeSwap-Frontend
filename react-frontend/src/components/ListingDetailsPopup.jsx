fe2a0bc01d6961607e19737dd81a85d912cf75dc
cafreyap <cafreyap@gmail.com>
2024-04-06 21:03:34 -0400

    fix

--- react-frontend/src/components/ListingDetailsPopup.jsx
+++ react-frontend/src/components/ListingDetailsPopup.jsx
@@ -1,28 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { BACKEND_ADDRESS } from './config';
+import React, { useState, useEffect } from "react";
+import { BACKEND_ADDRESS } from "./config";
 
 function ListingDetailsPopup({ listing, onClose }) {
   const [userEmail, setUserEmail] = useState(null);
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Track user's login status
 
-  // Function to fetch user email
   const fetchUserEmail = async () => {
     try {
-      const response = await fetch(`${BACKEND_ADDRESS}/api/get_user_email/`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          // Add any necessary authentication headers if required
-        },
-        credentials: 'include', // Include credentials in the request
-      });
+      const response = await fetch(
+        `${BACKEND_ADDRESS}/api/get_user_email/?username=${listing.username}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            // Add any necessary authentication headers if required
+          },
+          credentials: "include", // Include credentials in the request
+        }
+      );
       const data = await response.json();
       setUserEmail(data.email); // Assuming the response contains an email field
     } catch (error) {
-      console.error('Error fetching user email:', error);
+      console.error("Error fetching user email:", error);
     }
   };
-  
 
   // Function to check if user is logged in
   const checkLoggedIn = () => {
@@ -45,17 +46,25 @@ function ListingDetailsPopup({ listing, onClose }) {
       <p className="popup-card-text">by: {listing.authors}</p>
       <p className="popup-card-text">listed by: {listing.username}</p>
       <p className="popup-card-text">{listing.description}</p>
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
         <img src={listing.thumbnail} alt={listing.title} />
         {isLoggedIn && ( // Render the button only if user is logged in
-          <button style={{ marginTop: '10px' }} className="view-email-button" onClick={fetchUserEmail}>
+          <button
+            style={{ marginTop: "10px" }}
+            className="view-email-button"
+            onClick={fetchUserEmail}
+          >
             View Email
           </button>
         )}
       </div>
-      {userEmail && (
-        <p className="popup-card-text">User Email: {userEmail}</p>
-      )}
+      {userEmail && <p className="popup-card-text">User Email: {userEmail}</p>}
     </div>
   );
 }
